132229c3774c417caeea0397b45acbd40c514f12
react <zoranobr@yahoo.com>
2019-05-23 20:06:20 +0200

    novi fajl

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+// function printName (text, name) {
+
+// 	console.log (arguments)
+
+// }
+// printName ("His name is ", "Peter");
+// printName ("His name was ", "Mike");
+
+
+// var printName = function() {
+
+// 	console.log("HIs name is Peter")
+
+// };
+
+// console.log ();
+
+
+// var calculateAge = function(curentYear, yearOfBirth) {
+
+// 	return curentYear - yearOfBirth;
+
+// };
+
+// var calculateYearOfBirth = function (curentYear, age) {
+// return curentYear - age;
+
+// }
+// var cYear = 2019;
+// console.log(calculateYearOfBirth(cYear, calculateAge(cYear, calculateAge(cYear, 1963))));
+
+
